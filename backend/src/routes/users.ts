cd931114e4404b7304a99b053356a2e045409e16
AbolfazlAdhami <aadhami880@gmail.com>
2026-04-26 03:15:36 +0330

    feat: user router done

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -2,6 +2,53 @@ import express, { Request, Response } from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
 import { check, validationResult } from "express-validator";
-// TODO: verify token middleware
+import verifyToken from "../middleware/auth";
+import { registerValidation } from "../shared/validation";
 
- 
+const router = express.Router();
+
+router.get("/me", verifyToken, async (req, res) => {
+  const { userId } = req;
+  try {
+    const user = await User.findById(userId).select("-password");
+    if (!user) return res.status(400).json({ message: "User not found" });
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went Wrong! try later." });
+  }
+});
+
+router.post("/register", registerValidation, async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ message: errors.array() });
+  }
+
+  try {
+    let user = await User.findOne({ email: req.body.email });
+
+    if (user) return res.status(400).json({ message: "User already exists" });
+
+    user = new User(req.body);
+    await user.save();
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY as string, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV == "production",
+      maxAge: 86400000,
+    });
+    return res.status(200).json({ message: "User registered Successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went Wrong! try later." });
+  }
+});
+
+
+export default router;
---
+++ backend/src/shared/validation.ts
@@ -0,0 +1,10 @@
+import { check } from "express-validator";
+
+const registerValidation = [
+  check("firstName", "First Name is Required!").isString(),
+  check("lastName", "Last Name is Required!").isString(),
+  check("email", "Email is required").isEmail(),
+  check("password", "Password with 6 or more characters required").isLength({ min: 8 }),
+];
+
+export { registerValidation };
