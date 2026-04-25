d49d6e8643be61f2590cfbf990fd5eab114d920f
AbolfazlAdhami <aadhami880@gmail.com>
2026-04-25 04:48:52 +0330

    feat: set some middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,32 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import path from "path";
 import { v2 as cloudinary } from "cloudinary";
+import mongoose from "mongoose";
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 8000;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.listen(PORT, () => {
   console.log(`✅ Server is running on http://localhost:${PORT}`);
 });
+
+
