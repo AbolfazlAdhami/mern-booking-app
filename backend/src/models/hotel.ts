803358bf175fa25b851cb33a576fd512ad735954
AbolfazlAdhami <aadhami880@gmail.com>
2026-04-26 01:49:49 +0330

    new: schema model doen

---
+++ backend/src/models/hotel.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { BookingType, HotelType } from "../shared/types";
+
+const bookingSchema = new mongoose.Schema<BookingType>({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  adultCount: { type: Number, required: true },
+  childCount: { type: Number, required: true },
+  checkIn: { type: Date, required: true },
+  checkOut: { type: Date, required: true },
+  totalCount: { type: Number, required: true },
+});
+
+const hotelSchema = new mongoose.Schema<HotelType>({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  city: { type: String, required: true },
+  description: { type: String, required: true },
+  type: { type: String, required: true },
+  adultCount: { type: Number, required: true },
+  childCount: { type: Number, required: true },
+  facilities: [{ type: String, required: true }],
+  pricePerNight: { type: Number, required: true },
+  starRating: { type: Number, required: true },
+  imageUrl: [{ type: String, required: true }],
+  lastUpdated: { type: Date, required: true },
+  booking: [bookingSchema],
+});
+
+const Hotel = mongoose.model<HotelType>("Hotel", hotelSchema);
+export default Hotel;
