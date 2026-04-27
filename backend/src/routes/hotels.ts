f4a4de7a0a38227f4c1379b7ff8573aba05b755a
AbolfazlAdhami <aadhami880@gmail.com>
2026-04-27 18:56:28 +0330

    feat: search and get hotels route

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -10,10 +10,51 @@ const router = express.Router();
 router.get("/search", async (req: Request, res: Response) => {
   try {
     const query = constructSearchQuery(req.query);
-  } catch (error) {}
+    let sortOptions = {};
+
+    switch (req.query.sortOption) {
+      case "starRating":
+        sortOptions = { starRating: -1 };
+        break;
+      case "pricePerNightAsc":
+        sortOptions = { pricePerNight: 1 };
+        break;
+      case "pricePerNightDesc":
+        sortOptions = { pricePerNight: -1 };
+        break;
+    }
+
+    const pageSize = 5;
+    const pageNumber = parseInt(req.query.page ? req.query.page.toString() : "1");
+    const skip = (pageNumber - 1) * pageSize;
+    const hotels = await Hotel.find(query).sort(sortOptions).skip(skip).limit(pageSize);
+
+    const total = await Hotel.countDocuments(query);
+
+    const response = {
+      data: hotels,
+      pagination: {
+        total,
+        page: pageNumber,
+        pages: Math.ceil(total / pageSize),
+      },
+    };
+    return res.json(response);
+  } catch (error) {
+    console.log("errors", error);
+    return res.status(500).json({ message: "Somethings went wrong!" });
+  }
 });
 
-router.get("/", async (req: Request, res: Response) => {});
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const hotels = await Hotel.find().sort("-lastUpdated");
+    return res.json({ data: hotels });
+  } catch (error) {
+    console.log("errors", error);
+    return res.status(500).json({ message: "Somethings went wrong!" });
+  }
+});
 
 router.get("/:id", async (req: Request, res: Response) => {});
 
