4aa9d967d674c7fc1fb74d2a93415382fc7594ae
AbolfazlAdhami <aadhami880@gmail.com>
2026-06-19 02:51:41 +0330

    fix: fix pre save on User schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,12 +13,12 @@ import myHotelsRoutes from "./routes/my-hotels";
 import bookingRoutes from "./routes/my-booking";
 
 const PORT = process.env.PORT || 8000;
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+console.log(process.env.MONGODB_CONNECTION_STRING);
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -9,11 +9,10 @@ const userSchema = new mongoose.Schema<UserType>({
   lastName: { type: String, required: true },
 });
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 8);
   }
-  next();
 });
 
 const User = mongoose.model<UserType>("User", userSchema);
